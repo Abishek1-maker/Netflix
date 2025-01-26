579c988b2ab29279f507a4bb1cb7a4499175b7aa
Abishek1-maker <kesu.kunwar@gmail.com>
2025-01-26 18:17:25 +0545

    tailwind confi done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Netflixlog: "netflix-font"
+      }
+    },
   },
   plugins: [],
 }
--- tailwind.js
+++ tailwind.js
@@ -1,8 +1,13 @@
-module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+ let email=document.getElementById('email');
+ let error=document.getElementById('error-email');
+//blur event to check if value exists or not
+
+ email.addEventListener('blur',()=>{
+if (email.value===""){
+ error.textContent="please enter the valid email"
+}
+else{
+  error.textContent=""
+}
+
+ })
